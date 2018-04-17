bdf6793626c62b74b142ac1ec6894850a545917b
ebcrowder <ebcrowder@gmail.com>
2018-04-17 08:30:46 -0700

    component routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<div />, document.getElementById('root'));
+import Login from './components/Login';
+import Summary from './components/Summary';
+import Account from './components/Account';
+
+import 'bulma/css/bulma.css';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Switch>
+        <Route path="/account" component={Account} />
+        <Route path="/summary" component={Summary} />
+        <Route path="/" component={Login} />
+      </Switch>
+    </div>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 registerServiceWorker();
